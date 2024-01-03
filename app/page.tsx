f5669d658a002c9295b2c5bd832a059caff1908e
Felippe Vieira <felippe.vieira@stgen.com>
2024-01-02 21:11:33 -0300

    project finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,57 @@
-import Image from 'next/image'
+'use client'
+import Image from "next/image";
+import React from "react";
 
 export default function Home() {
+  const [rating, setRating] = React.useState(0);
+  const [submitted, setSubmitted] = React.useState(false);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const rating = formData.get('rating');
+    setRating(Number(rating));
+    setSubmitted(true);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-slate-950 h-screen grid justify-center items-center p-8">
+      {!submitted && (
+        <form onSubmit={handleSubmit} className="bg-gradient-to-b from-slate-800 to-slate-900 max-w-96 rounded-lg flex flex-col gap-6 p-5">
+          <span className="rounded-full bg-slate-700 flex items-center justify-center h-12 w-12">
+            <Image src="/icon-star.svg" alt="star" width="20" height="20" />
+          </span>
+          <h1 className="text-white text-2xl font-bold">How did we do?</h1>
+          <p className="text-slate-400">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+          <div className="flex flex-wrap justify-between gap-6">
+            <input className="hidden peer/one" type="radio" id="one" name="rating" value="1" />
+            <label className="rounded-full bg-slate-700 flex items-center justify-center h-12 w-12 cursor-pointer hover:bg-orange-500 hover:text-white text-slate-400 peer-checked/one:bg-slate-400 peer-checked/one:text-white" htmlFor="one">1</label>
+
+            <input className="hidden peer/two" type="radio" id="two" name="rating" value="2" />
+            <label className="rounded-full bg-slate-700 flex items-center justify-center h-12 w-12 cursor-pointer hover:bg-orange-500 hover:text-white text-slate-400 peer-checked/two:bg-slate-400 peer-checked/two:text-white" htmlFor="two">2</label>
+
+            <input className="hidden peer/three" type="radio" id="three" name="rating" value="3" />
+            <label className="rounded-full bg-slate-700 flex items-center justify-center h-12 w-12 cursor-pointer hover:bg-orange-500 hover:text-white text-slate-400 peer-checked/three:bg-slate-400 peer-checked/three:text-white" htmlFor="three">3</label>
+
+            <input className="hidden peer/four" type="radio" id="four" name="rating" value="4" />
+            <label className="rounded-full bg-slate-700 flex items-center justify-center h-12 w-12 cursor-pointer hover:bg-orange-500 hover:text-white text-slate-400 peer-checked/four:bg-slate-400 peer-checked/four:text-white" htmlFor="four">4</label>
+
+            <input className="hidden peer/five" type="radio" id="five" name="rating" value="5" />
+            <label className="rounded-full bg-slate-700 flex items-center justify-center h-12 w-12 cursor-pointer hover:bg-orange-500 hover:text-white text-slate-400 peer-checked/five:bg-slate-400 peer-checked/five:text-white" htmlFor="five">5</label>
+          </div>
+          <button className="bg-orange-500 rounded-full text-white font-semibold py-2 px-3 uppercase tracking-widest hover:bg-white hover:text-orange-500" type="submit">Submit</button>
+        </form>
+      )}
+      {submitted && (
+        <div className="bg-gradient-to-b from-slate-800 to-slate-900 max-w-96 rounded-lg flex flex-col items-center gap-8 px-10 py-10">
+          <Image src="/illustration-thank-you.svg" alt="thank you" width="150" height="150" />
+          <span className="text-orange-500 rounded-full bg-slate-800 px-6 py-2 text-sm">You selected {rating} out of 5</span>
+          <div className="text-center flex flex-col gap-4">
+            <h1 className="text-white text-2xl font-bold mt-4">Thank you!</h1>
+            <p className="text-slate-400 text-sm">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
   )
 }
